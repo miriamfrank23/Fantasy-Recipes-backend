05da3eb72d85d081a53cc21cc1d37de6268dc7b7
miriamfrank23 <miriamfrank23@gmail.com>
2018-10-24 09:57:44 -0400

    added clicks column to worlds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_191335) do
+ActiveRecord::Schema.define(version: 2018_10_24_135601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2018_10_22_191335) do
     t.string "banner"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "clicks"
   end
 
 end
