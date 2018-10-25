7a0557c6933f8adbee3984db3a0145c698519912
miriamfrank23 <miriamfrank23@gmail.com>
2018-10-25 14:01:16 -0400

    added 1 good seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,9 @@ World.create(name: "The Lord of the Rings", image: "https://i.imgur.com/Xxivd9P.
 puts "End Seed Star Wars"
 puts "Start Seed Star Wars Recipes"
 
-Recipe.create(name: "Jaba the Hutt Cake", image: "https://i.imgur.com/eEmD8gE.jpg", instructions: "cook stuff", world_id: 1)
+Recipe.create(name: "Jaba the Hutt Cake", image: "https://i.imgur.com/eEmD8gE.jpg", instructions: 'Preheat oven to 180C. Lightly grease and line a 23cm square pan with baking paper.
+In a medium saucepan, combine dark chocolate and butter on medium heat, stirring until melted. Stir in sugar. Whisk in eggs, stir through flour and vanilla extract. Bake 55-60 minutes. Make marshmallow icing: Combine sugar and water; beat for 4 minutes. Dissolve gelatine in the hot water, then beat hot gelatine mixture into sugar mixture. Add the vanilla extract. Tint marshmallow yellow. Paint green food colouring into inside of piping bag, leaving one side unpainted. Fill bag with marshmallow and pipe body and tail of Jabba onto top of brownie. Attach halved jubes to white chocolate melts, and halved mini M&M to top of orange jube. Press eyes on top of marshmallow. While marshmallow icing is still wet, use skewer to mark out the nostrils and mouth. Using a thin paintbrush and a little brown colouring, fill in nose and mouth. Roll out orange fondant to 3mm thick. Using round cutter, cut out 4 rounds. Attach round chocolates to the top of each round, and press onto front edge of brownie cake, using a little marshmallow to affix.
+', world_id: 1)
 Recipe.create(name: "Yoda Cupcakes", image: "https://i.imgur.com/iAS4rNY.jpg", instructions: "cook stuff", world_id: 1)
 Recipe.create(name: "Darth Vader Candy", image: "https://i.imgur.com/t9GzHlU.jpg", instructions: "cook stuff", world_id: 1)
 Recipe.create(name: "Porg Cookies", image: "https://i.imgur.com/2ecHYMl.jpg", instructions: "cook stuff", world_id: 1)
@@ -42,9 +44,13 @@ Recipe.create(name: "Bag End Cupcakes", image: "https://i.imgur.com/92f55rU.jpg"
 puts "End Seed Lord of the Rings Recipes"
 puts "Start Seed Ingredients"
 
-Ingredient.create(name: "Chocolate", quantity: "2 cups Chocolate", recipe_id: 1)
-Ingredient.create(name: "Dough", quantity: "2 cups Dough", recipe_id: 1)
-Ingredient.create(name: "Pretzel", quantity: "Bag of Sticks", recipe_id: 1)
+Ingredient.create(name: "Chocolate", quantity: "370 g chocolate", recipe_id: 1)
+Ingredient.create(name: "Butter", quantity: "250 g butter", recipe_id: 1)
+Ingredient.create(name: "Sugar", quantity: "2 cups sugar", recipe_id: 1)
+Ingredient.create(name: "Eggs", quantity: "4 eggs", recipe_id: 1)
+Ingredient.create(name: "Flour", quantity: "2 cups flour", recipe_id: 1)
+Ingredient.create(name: "Vanilla", quantity: "2 tsp vanilla extract", recipe_id: 1)
+
 Ingredient.create(name: "Chocolate", quantity: "2 cups Chocolate", recipe_id: 2)
 Ingredient.create(name: "Chocolate", quantity: "2 cups Chocolate", recipe_id: 2)
 Ingredient.create(name: "Dough", quantity: "2 cups Dough", recipe_id: 2)
